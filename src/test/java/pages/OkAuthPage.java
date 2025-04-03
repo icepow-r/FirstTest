81ea3c00ac7653c750fc730be545aa8a21c605fa
Ilia <icepower@internet.ru>
2025-04-03 15:36:18 +0300

    Extract locators

--- src/test/java/pages/OkAuthPage.java
+++ src/test/java/pages/OkAuthPage.java
@@ -14,6 +14,15 @@ public class OkAuthPage {
     private final SelenideElement errorMessage = $("div[class='input-e login_error']");
     private final SelenideElement restoreEmailErrorMessage = $("div[class='input-e']");
     private final SelenideElement restoreAccessMessage = $("div[class='stub']");
+    private final SelenideElement restoreAccessButton = $("a[data-l='t,restore']");
+    private final SelenideElement contactSupportButton = $("a[tsid='support-link_link_5998d4']");
+    private final SelenideElement recoveryEmailButton = $("a[data-l='t,email']");
+    private final SelenideElement getCodeButton = $("input[tsid='recovery-start-email-verification-block_input_7f2bff']");
+    private final SelenideElement problemSelector = $("#categorynew");
+    private final SelenideElement subjectSelector = $("#subcategory");
+    private final SelenideElement categorySelector = $("#categoryValue");
+    private final SelenideElement lookupSelector = $("#lookup");
+    private final SelenideElement emailField = $("#field_e-mail");
 
     public OkAuthPage open() {
         Selenide.open("https://ok.ru");
@@ -60,31 +69,31 @@ public class OkAuthPage {
     }
 
     public OkAuthPage checkContactSupportFormFields() {
-        $("#categorynew").shouldBe(visible);
-        $("#subcategory").shouldBe(visible);
-        $("#categoryValue").shouldBe(visible);
-        $("#lookup").shouldBe(visible);
-        $("#field_e-mail").shouldBe(visible);
+        problemSelector.shouldBe(visible);
+        subjectSelector.shouldBe(visible);
+        categorySelector.shouldBe(visible);
+        lookupSelector.shouldBe(visible);
+        emailField.shouldBe(visible);
         return this;
     }
 
     public OkAuthPage restoreAccessButtonClick() {
-        $("a[data-l='t,restore']").shouldBe(visible).click();
+        restoreAccessButton.shouldBe(visible).click();
         return this;
     }
 
     public OkAuthPage contactSupportButtonClick() {
-        $("a[tsid='support-link_link_5998d4']").shouldBe(visible).click();
+        contactSupportButton.shouldBe(visible).click();
         return this;
     }
 
     public OkAuthPage emailButtonClick() {
-        $("a[data-l='t,email']").shouldBe(visible).click();
+        recoveryEmailButton.shouldBe(visible).click();
         return this;
     }
 
     public OkAuthPage getCodeButtonClick() {
-        $("input[tsid='recovery-start-email-verification-block_input_7f2bff']").shouldBe(visible).click();
+        getCodeButton.shouldBe(visible).click();
         return this;
     }
 }
