cf187d7c5c8df0e45c4d08fc41390ea9f3d6aef1
Ilia <icepower@internet.ru>
2025-04-17 17:09:58 +0300

    Update naming

--- src/test/java/pages/SupportPage.java
+++ src/test/java/pages/SupportPage.java
@@ -6,17 +6,17 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SupportPage {
-    private final SelenideElement problemSelector = $("#categorynew");
-    private final SelenideElement subjectSelector = $("#subcategory");
-    private final SelenideElement categorySelector = $("#categoryValue");
-    private final SelenideElement lookupSelector = $("#lookup");
+    private final SelenideElement problemDropdown = $("#categorynew");
+    private final SelenideElement subjectDropdown = $("#subcategory");
+    private final SelenideElement categoryDropdown = $("#categoryValue");
+    private final SelenideElement lookupDropdown = $("#lookup");
     private final SelenideElement emailField = $("#field_e-mail");
 
     public SupportPage checkContactSupportFormFields() {
-        problemSelector.shouldBe(visible);
-        subjectSelector.shouldBe(visible);
-        categorySelector.shouldBe(visible);
-        lookupSelector.shouldBe(visible);
+        problemDropdown.shouldBe(visible);
+        subjectDropdown.shouldBe(visible);
+        categoryDropdown.shouldBe(visible);
+        lookupDropdown.shouldBe(visible);
         emailField.shouldBe(visible);
         return this;
     }
